bcaa50d3cc98ed2933f3772027f8403c6a74ec15
Mirza Delibasic <mdelibasic1@etf.unsa.ba>
2018-05-03 16:16:35 +0200

    Kreiranje klasa Administrator (View, Model, ModelView, DB)

---
+++ Projekat2/eRouting2/eRouting2/DB/DBConnectionString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace eRouting2
+{
+    public class DBConnectionString
+    {
+        private String s;
+        public DBConnectionString()
+        {
+            s = "Server=tcp:ooad-men.database.windows.net,1433;Initial Catalog=OOADBaza;Persist Security Info=False;User ID=user;Password=ooad;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
+        public String GetString() { return s; }
+    }
+}
---
+++ Projekat2/eRouting2/eRouting2/ViewModel/VMAdministrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRouting2
+{
+    public class VMAdministrator
+    {
+        public Administrator Admin { get; set; }
+        public void ObrisiKorisnika(String username)
+        {
+            
+        }
+        public void PrikaziStatistiku(String username)
+        {
+
+        }
+
+    }
+}
